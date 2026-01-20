29c06bbb681e13ab048b766cb94742d7feac4177
jasonstalyn90-sys <jasonstalyn90@gmail.com>
2026-01-20 02:03:43 +0000

    wip: finally commit

--- semana3/script_s3.sh
+++ semana3/script_s3.sh
@@ -1,3 +1,14 @@
 ls -l /home
 touch 'script s3'
 cat script\ s3
+echo Hola Unix
+printf "Hola Unix\n"
+echo Hola Unix1
+echo Hola Unix2
+printf "Hola %s\n" "UNIX"
+ps -a -u -x
+ps -aux
+mkdir test
+ls
+rmdir test
+ls
