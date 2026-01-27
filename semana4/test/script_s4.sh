615479bb404b00ba39b90cf965befdf83c111e4f
jasonstalyn90-sys <jasonstalyn90@gmail.com>
2026-01-27 01:56:58 +0000

    wp: cp and mv

---
+++ semana4/test/script_s4.sh
@@ -0,0 +1,10 @@
+mkdir -p padre_jo/hijo_jo
+pwd
+cd 
+pwd
+cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/semana4/padre_jo/hijo_jo
+touch archivo_jo.txt
+echo '0' > archivo_jo.txt
+echo '1' >> archivo_jo.txt
+echo '2' >> archivo_jo.txt
+echo '3' >> archivo_jo.txt
